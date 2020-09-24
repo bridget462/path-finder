5382402c517c336745f2785f9128ebe0c64d24d6
bridget462 <bridges.dev.create@gmail.com>
2020-09-24 19:06:26 +0900

    PathFinder and Node component created

---
+++ src/components/Node.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Node = () => {
+  return (
+    <div>
+      <h2>This is Node!</h2>
+    </div>
+  );
+};
+
+export default Node;
---
+++ src/components/PathFinder.js
@@ -0,0 +1,16 @@
+import React from "react";
+// components
+import Node from "./Node";
+
+class PathFinder extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is PathFinder!</h1>
+        <Node />
+      </div>
+    );
+  }
+}
+
+export default PathFinder;
